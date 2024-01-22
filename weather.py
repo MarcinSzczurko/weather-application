fd228c7ac1af383cb178e085d63981cdd0c02f8b
Marcin Szczurko <email@email.com>
2024-01-22 13:02:29 +0100

    Add read_user_cli_args() function

--- weather.py
+++ weather.py
@@ -1,4 +1,24 @@
+import argparse
 from configparser import ConfigParser
+
+
+def read_user_cli_args():
+    """Handles the CLI user interactions.
+
+    Returns:
+        argparse.Namespace: Populated namespace object
+    """
+    parser = argparse.ArgumentParser(
+        description="Gets weather and temperature information for a city"
+    )
+    parser.add_argument("city", nargs="+", type=str, help="Enter the city name")
+    parser.add_argument(
+        "-f",
+        "--fahrenheit",
+        action="store_true",
+        help="Display the temperature in fahrenheit",
+    )
+    return parser.parse_args()
 
 
 def _get_api_key():
@@ -12,3 +32,7 @@ def _get_api_key():
     config = ConfigParser()
     config.read("secrets.ini")
     return config["openweather"]["api_key"]
+
+
+if __name__ == "__main__":
+    read_user_cli_args()
